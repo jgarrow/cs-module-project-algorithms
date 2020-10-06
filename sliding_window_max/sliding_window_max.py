94f2636c1e7d68544394c544c2652043445acc1b
Janessa Garrow <janessa.garrow@gmail.com>
2020-10-05 21:49:02 -0600

    Complete sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,37 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    max_arr = []
+    curr_index = 0
 
-    pass
+    # loop over nums
+    # for each num_index, make a new array that slices nums[num_index, k]
+    # then loop over sliced nums for max total
+
+    while curr_index < len(nums):
+        # initialize to None to handle potential negative integers
+        curr_slice_max = None
+
+        # current window slice
+        curr_arr_slice = []
+
+        # the last array must start at k indices from the end
+        if curr_index <= len(nums) - k:
+            curr_arr_slice = nums[curr_index:curr_index + k]
+        
+            # get largest all nums in our current window slice
+            for num in curr_arr_slice:
+                if curr_slice_max is None:
+                    curr_slice_max = num
+                elif num > curr_slice_max:
+                    curr_slice_max = num
+            
+            max_arr.append(curr_slice_max)
+        
+        curr_index += 1
+    
+    return max_arr
+        
 
 
 if __name__ == '__main__':
