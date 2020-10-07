015244c59245020499068670cbb8f83bc6126105
Janessa Garrow <janessa.garrow@gmail.com>
2020-10-06 21:10:17 -0600

    Complete eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,43 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+def eating_cookies(n):
+
+    # total = 0
+
+    # check if n - 3 > 0
+        # total += eating_cookeis(n - 3)
+
+    # check if n - 2 > 0
+        # total += eating_cookeis(n - 2)
+
+    # check if n - 1 > 0
+        # total += eating_cookeis(n - 1)
+
+
+    # if n == 1,2,3
+        # add 1 to total
+    
+    total = 0
+    
+    if n == 0:
+        return 1
+
+    if n - 3 > 0:
+        total += eating_cookies(n - 3)
+    
+    if n - 2 > 0:
+        total += eating_cookies(n - 2)
+    
+    if n - 1 > 0:
+        total += eating_cookies(n - 1)
+    
+    if n == 1 or n == 2 or n == 3:
+        total += 1
+
+    return total
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
